b2e0f11ea39027f71ebc50fa3f67b6e5394a07e0
Dang Anh Hao <danganhhao8a3vts@gmail.com>
2020-05-10 10:34:54 +0700

    code add data

---
+++ project/api/rundata.py
@@ -0,0 +1,132 @@
+"""
+python manage.py shell < api/rundata.py
+
+"""
+import cloudinary.uploader
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser
+from rest_framework.response import Response
+
+from api.models import *
+from api.serializers import *
+from user.helper.authentication import Authentication
+from user.helper.json import create_json_response
+from user.helper.utils import *
+from user.models import *
+from user.helper.string import *
+
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.utils import timezone
+
+import csv
+
+# ------------------- Get Parameters ---------------------#
+user_id = 28
+
+print("AAA")
+reader = csv.DictReader(open("/home/danganhhaotest/RealEstate_server/realestate_data_100.csv"))
+for raw in reader:
+    title = raw.get('title')  # required
+    estateType = raw.get('estateType')  # required
+    expire_after = raw.get('expireAfter')  # required
+    project = raw.get('project', None)
+    province = raw.get('province')  # required
+    district = raw.get('district')  # required
+    ward = raw.get('ward', None)
+    street = raw.get('street', None)
+    address_detail = raw.get('addressDetail', "")
+    numberOfRoom = raw.get('numberOfRoom', "")
+    description = raw.get('description')  # required
+    detail = raw.get('detail')
+    price = raw.get('price', "")
+    area = raw.get('area', "")
+    contact = raw.get('contact', "")
+    images = raw.get('image', "")
+    transaction = raw.get('transaction')  # required
+    lat = raw.get('lat', "")
+    lng = raw.get('lng', "")
+
+    try:
+        # ------------------- Create Estate ---------------------#
+
+        # ------------------- Normalizer data -------------------#
+        project_instance = None
+        ward_instance = None
+        street_instance = None
+        estateType_instance = EstateType.objects.get(id=estateType)
+        estateStatus_instance = EstateStatus.objects.get(id=10)
+        province_instance = Province.objects.get(id=province)
+        district_instance = District.objects.get(id=district)
+        expireDays = int(expire_after)
+        if project:
+            project_instance = Project.objects.get(id=project)
+        if ward:
+            ward_instance = Ward.objects.get(id=ward)
+        if street:
+            street_instance = Street.objects.get(id=street)
+        if numberOfRoom == "":
+            numberOfRoom = 0
+        if price == "":
+            price = 0
+        if area == "":
+            area = 0
+        if expireDays > 365:
+            expireDays = 365
+
+        # ------------------------------------------------#
+        estate = Estate(
+            title=title,
+            estateType=estateType_instance,
+            estateStatus=estateStatus_instance,
+            project=project_instance,
+            province=province_instance,
+            district=district_instance,
+            ward=ward_instance,
+            addressDetail=address_detail,
+            street=street_instance,
+            numberOfRoom=numberOfRoom,
+            description=description,
+            detail=detail,
+            price=price,
+            area=area,
+            contact=contact,
+            created_day=timezone.now(),
+            lat=lat,
+            lng=lng
+        )
+        estate.save()
+
+        # ------------------- Create Image ---------------------#
+
+        estate_id = estate.id
+        if images:
+            path = uploadLocationEstate(estate_id, len(images))
+            upload_data = cloudinary.uploader.upload(images, public_id=path)
+            estate_image = EstateImage(
+                estate=estate,
+                image=upload_data['secure_url']
+            )
+            estate_image.save()
+
+        # ------------------- Create Post ---------------------#
+        user_instance = User.objects.get(id=user_id)
+        transaction_instance = TransactionType.objects.get(id=transaction)
+        new_post = Post(
+            user=user_instance,
+            estate=estate,
+            transaction=transaction_instance,
+            dateFrom=timezone.now(),
+            dateTo=(timezone.now() + timezone.timedelta(days=expireDays))
+        )
+        new_post.save()
+
+        print('Done - ' + str(estate.id))
+
+    except EstateType.DoesNotExist:
+        error_header = {'error_code': EC_FAIL, 'error_message': ' fail'}
+
+
+if __name__ == "__main__":
+    print("AA")
