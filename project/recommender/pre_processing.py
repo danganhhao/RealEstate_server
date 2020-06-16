f818f9ee5188b347dd886837374e8b9d1e1386fe
1612543 <quitland2010@gmail.com>
2020-06-16 12:11:41 +0700

    optimize sim_matrix

--- project/recommender/pre_processing.py
+++ project/recommender/pre_processing.py
@@ -1,10 +1,5 @@
-import csv
-import math
-
 from api.models import Estate
-import pandas as pd
 import numpy as np
-from scipy import spatial
 
 ID_IDX = 0
 ESTATE_TYPE_IDX = ID_IDX + 1
@@ -37,7 +32,7 @@ def get_estates_from_server():
 
 
 def convert_estate_to_array(estate):
-    item = [None] * (AREA_IDX + 1)
+    item = [None] * 8
     item[ID_IDX] = estate.id
     item[ESTATE_TYPE_IDX] = estate.estateType.get_id()
     item[PROVINCE_IDX] = estate.province.get_id()
@@ -53,33 +48,22 @@ def arrange_estate_to_group(estatesData):
     res = {}
     n_estate = len(estatesData)
     for i in range(0, n_estate):
-        estateInArray = convert_estate_to_array(estatesData[i])
-
-        provinceId = estateInArray[PROVINCE_IDX]
-        districtId = estateInArray[DISTRICT_IDX]
-        estateType = estateInArray[ESTATE_TYPE_IDX]
-        transactionType = estateInArray[TRANSACTION_IDX]
-        if provinceId not in res:
-            res[provinceId] = {}
-
-        if districtId not in res[provinceId]:
-            res[provinceId][districtId] = {}
-
-        if estateType not in res[provinceId][districtId]:
-            res[provinceId][districtId][estateType] = {}
-
-        if transactionType not in res[provinceId][districtId][estateType]:
-            res[provinceId][districtId][estateType][transactionType] = []
-
-        res[provinceId][districtId][estateType][transactionType].append(
-            [
-                estateInArray[ID_IDX],
-                estateInArray[NUM_OF_ROOM_IDX],
-                estateInArray[PRICE_IDX],
-                estateInArray[AREA_IDX],
-            ]
-        )
-
+        estate_in_array = convert_estate_to_array(estatesData[i])
+        province_id = estate_in_array[PROVINCE_IDX]
+        district_id = estate_in_array[DISTRICT_IDX]
+        estate_type = estate_in_array[ESTATE_TYPE_IDX]
+        transaction_type = estate_in_array[TRANSACTION_IDX]
+        if province_id not in res:
+            res[province_id] = {}
+        if district_id not in res[province_id]:
+            res[province_id][district_id] = {}
+        if estate_type not in res[province_id][district_id]:
+            res[province_id][district_id][estate_type] = {}
+        if transaction_type not in res[province_id][district_id][estate_type]:
+            res[province_id][district_id][estate_type][transaction_type] = []
+        res[province_id][district_id][estate_type][transaction_type].append(
+            [estate_in_array[ID_IDX], estate_in_array[NUM_OF_ROOM_IDX], estate_in_array[PRICE_IDX],
+             estate_in_array[AREA_IDX]])
     return res
 
 
@@ -101,31 +85,38 @@ def calc_sim_matrix(listEstate):
 
 
 def save_object_sim_data_to_file(estatesByLocation):
-    # TODO: save object to txt file
-    pass
+    with open('sim_matrix_data.txt', 'w') as f:
+        print(estatesByLocation, file=f)
 
 
+# save_object_sim_data_to_file({3: {10: 11}})
+
+print("\n[Start get data from server]\n")
 estatesData = get_estates_from_server()
+print("\n------- Done ------------\n")
+
+print("\n[Arrange estate to group]\n")
 estatesByLocation = arrange_estate_to_group(estatesData)
+print("\n------- Done ------------\n")
+# print('estatesByLocation', estatesByLocation)
 
-"""
-structure of estatesByLocation:
-{
-    1: {                            //provinceId
-        101: {                      //districtId
-            4: {                    //estateType
-                0: [                //transactionType
-                    [10001, 0, 20000, 8],  //[id, numRoom, price, area]
-                    [10002, 0, 20000, 8],
-                ]            
-            }  
-        }
-    }
-    .
-    .
-}
-"""
+# structure of estatesByLocation:
+# {
+#     1: {                            //provinceId
+#         101: {                      //districtId
+#             4: {                    //estateType
+#                 0: [                //transactionType
+#                     [10001, 0, 20000, 8],  //[id, numRoom, price, area]
+#                     [10002, 0, 20000, 8],
+#                 ]
+#             }
+#         }
+#     }
+#     .
+#     .
+# }
 
+print("\n[Calc sim matrix]\n")
 for provinceId in estatesByLocation:
     estateInProvince = estatesByLocation[provinceId]
     for districtId in estateInProvince:
@@ -134,87 +125,29 @@ for provinceId in estatesByLocation:
             estateSameType = estateInDistrict[estateType]
             for transactionType in estateSameType:
                 estateArraySameLocation = estateSameType[transactionType]  # [[id, numRoom, price, area]]
-
                 listEstateIdSameLocation = get_list_estate_id(estateArraySameLocation)  # pop first element of each estate
                 simMatrix = calc_sim_matrix(estateArraySameLocation)
-                estateSameType[transactionType] = {
-                    "sim_matrix": simMatrix,
-                    "list_estate_id": listEstateIdSameLocation
-                }
+                estatesByLocation[provinceId][districtId][estateType][transactionType] = {"sim_matrix": simMatrix,"list_estate_id": listEstateIdSameLocation}
 
+print("\n------- Done ------------\n")
+
+print("\n[Save to file]\n")
 save_object_sim_data_to_file(estatesByLocation)
-"""
-structure of estatesByLocation after process:
-{
-    1: {                            //provinceId
-        101: {                      //districtId
-            4: {                    //estateType
-                0: {                //transactionType
-                    "sim_matrix": 2D array,
-                    "list_estate_id": array, list estate id in this location
-                }            
-            }  
-        }
-    }
-    .
-    .
-}
-"""
+print("\n------- Done ------------\n")
+
+# structure of estatesByLocation after process:
+# {
+#     1: {                            //provinceId
+#         101: {                      //districtId
+#             4: {                    //estateType
+#                 0: {                //transactionType
+#                     "sim_matrix": 2D array,
+#                     "list_estate_id": array, list estate id in this location
+#                 }
+#             }
+#         }
+#     }
+#     .
+#     .
+# }
 #
-# print('start pre_processing')
-# table = []
-# idIndex = {}
-# estates = Estate.objects.all()
-# for idx in range(0, len(estates)):
-#     item = [None] * (AREA_IDX + 1)
-#     item[ID_IDX] = estates[idx].id
-#     item[ESTATE_TYPE_IDX] = estates[idx].estateType.get_id()
-#     item[PROVINCE_IDX] = estates[idx].province.get_id()
-#     item[DISTRICT_IDX] = estates[idx].district.get_id()
-#     item[TRANSACTION_IDX] = estates[idx].transaction.get_id()
-#     item[NUM_OF_ROOM_IDX] = estates[idx].numberOfRoom
-#     item[PRICE_IDX] = estates[idx].price
-#     item[AREA_IDX] = estates[idx].area
-#     table.append(item)
-#
-# data = np.asarray(table)
-# max_in_col = np.amax(data, axis=0)
-#
-# data[:, NUM_OF_ROOM_IDX] = np.true_divide(data[:, NUM_OF_ROOM_IDX], max_in_col[NUM_OF_ROOM_IDX])
-# data[:, AREA_IDX] = np.true_divide(data[:, AREA_IDX], max_in_col[AREA_IDX])
-# data[:, PRICE_IDX] = np.true_divide(data[:, PRICE_IDX], max_in_col[PRICE_IDX])
-#
-# n_estate = len(table)
-# sim_matrix_object = {}
-#
-# sim_matrix = np.zeros(shape=(n_estate, n_estate))
-# for i in range(0, n_estate):
-#     for j in range(0, n_estate):
-#         if check2EstateSameLocation(data[i], data[j]):
-#             provinceId = data[i][PROVINCE_IDX]
-#             districtId = data[i][DISTRICT_IDX]
-#             estateType = data[i][ESTATE_TYPE_IDX]
-#             transactionType = data[i][TRANSACTION_IDX]
-#             if provinceId not in sim_matrix_object:
-#                 sim_matrix_object[provinceId] = {}
-#
-#             if districtId not in sim_matrix_object[provinceId]:
-#                 sim_matrix_object[provinceId][districtId] = {}
-#
-#             if estateType not in sim_matrix_object[provinceId][districtId]:
-#                 sim_matrix_object[provinceId][districtId][estateType] = {}
-#
-#             if transactionType not in sim_matrix_object[provinceId][districtId][estateType]:
-#                 sim_matrix_object[provinceId][districtId][estateType][transactionType] = []
-#
-#             # dis = spatial.distance.cosine(data[i], data[j])
-#             dis = euclid_distance(data[i], data[j])
-#         else:
-#             dis = float('inf')
-#         sim_matrix[i, j] = dis
-#
-# pd.DataFrame(sim_matrix, dtype=np.float).to_csv("recommender/sim_matrix.csv", index=None, header=None)
-# with open('recommender/idIndex.csv', 'w') as csv_file:
-#     writer = csv.writer(csv_file)
-#     for key, value in idIndex.items():
-#         writer.writerow([key, value])
--- project/recommender/processing.py
+++ project/recommender/processing.py
@@ -25,18 +25,51 @@ def read_sim_matrix_file(file_name):
     return data
 
 
-def get_similar_items(item_id, number):
-    idIndex = read_idIndex_file("idIndex.csv")
-    sim_matrix = read_sim_matrix_file("sim_matrix.csv")
-    # idIndex = read_idIndex_file("recommender/idIndex.csv")
-    # sim_matrix = read_sim_matrix_file("recommender/sim_matrix.csv")
-    index = int([k for k, v in idIndex.items() if v == item_id][0])
-    ind = sim_matrix[index].argsort()[:number][::-1]
-    res = []
-    for i in ind:
-        res.append(idIndex[str(i)])
-    return res
+# def get_similar_items(item_id, number):
+#     idIndex = read_idIndex_file("idIndex.csv")
+#     sim_matrix = read_sim_matrix_file("sim_matrix.csv")
+#     # idIndex = read_idIndex_file("recommender/idIndex.csv")
+#     # sim_matrix = read_sim_matrix_file("recommender/sim_matrix.csv")
+#     index = int([k for k, v in idIndex.items() if v == item_id][0])
+#     ind = sim_matrix[index].argsort()[:number]
+#     res = []
+#     for i in ind:
+#         res.append(idIndex[str(i)])
+#     return res
+#
 
+def get_estate_info(item_id):
+    """
+    get data from server and
+    return [province_id, district_id, estate_type, transaction_type] of estate
+    """
+    return [1,1,1,1]
+
+
+def get_whole_sim_matrix_from_csv():
+    return {}
+
+
+def get_sim_matrix(estate_location_info):
+    """
+    estate_location_info: [province_id, district_id, estate_type, transaction_type]
+    return: {sim_matrix: 2D array, list_estate_id: []}
+    """
+    whole_sim_matrix = get_whole_sim_matrix_from_csv()
+    province_id = estate_location_info[0]
+    district_id = estate_location_info[1]
+    estate_type = estate_location_info[2]
+    transaction_type = estate_location_info[3]
+    return whole_sim_matrix[province_id][district_id][estate_type][transaction_type]
+
+def get_similar_items(item_id, k = 5):
+    res = []
+    estate_location_info = get_estate_info(item_id)
+    sim_matrix_object = get_sim_matrix(estate_location_info)
+    sim_matrix = sim_matrix_object["sim_matrix"]
+    id_index = sim_matrix_object["list_estate_id"]
+    # TODO: select most similar estate
+    return res
 
 if __name__ == "__main__":
     res = get_similar_items('68', 5)
